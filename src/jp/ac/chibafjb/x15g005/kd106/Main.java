56667014e9caf9c12d91f7da82857033ba1242c3
unknown <x15g005@PC504N017.chiba-fjb.ac.jp>
2015-11-02 14:59:54 +0900

    課題６

---
+++ src/jp/ac/chibafjb/x15g005/kd106/Main.java
@@ -0,0 +1,18 @@
+package jp.ac.chibafjb.x15g005.kd106;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("  a@@a a@@a");
+		System.out.println(" @@@@@a@@@@@");
+		System.out.println("(@@@@@@@@@@@)");
+		System.out.println(" @@@@@@@@@@@");
+		System.out.println("  @@@@@@@@@");
+		System.out.println("  ~@@@@@@@~");
+		System.out.println("    ~@@@~ ");
+		System.out.println("     ~@~");
+
+	}
+
+}
