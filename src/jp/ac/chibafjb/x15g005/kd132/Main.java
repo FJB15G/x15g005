63a07177422943bc05e3ce7dc16f971b52930f97
unknown <x15g005@PC504N017.chiba-fjb.ac.jp>
2015-11-09 14:29:26 +0900

    課題２２

---
+++ src/jp/ac/chibafjb/x15g005/kd132/Main.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g005.kd132;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		i=10;
+		System.out.print("x="+i);
+
+	}
+
+}
