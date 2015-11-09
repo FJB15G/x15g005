eff3dc7664bc8d64ab97a1a40960a5d526835961
unknown <x15g005@PC504N017.chiba-fjb.ac.jp>
2015-11-09 14:13:37 +0900

    課題１５

---
+++ src/jp/ac/chibafjb/x15g005/kd124/Main.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g005.kd124;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		for(i=0;i<10;i+=1)
+			System.out.print("☆");
+
+	}
+
+}
