680e8fb8b4214e12c10d2ebab7816a0cb168f125
unknown <x15g005@PC504N017.chiba-fjb.ac.jp>
2015-11-09 14:27:27 +0900

    課題２１

---
+++ src/jp/ac/chibafjb/x15g005/kd131/Main.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g005.kd131;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		i=10;
+		System.out.print(i);
+
+	}
+
+}
