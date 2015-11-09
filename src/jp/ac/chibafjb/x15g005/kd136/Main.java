00c0433a0ad56ce5d8b7ae90f052a6d889b73bcf
unknown <x15g005@PC504N017.chiba-fjb.ac.jp>
2015-11-09 14:52:13 +0900

    課題２６

---
+++ src/jp/ac/chibafjb/x15g005/kd136/Main.java
@@ -0,0 +1,21 @@
+package jp.ac.chibafjb.x15g005.kd136;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a;
+		int b;
+		int c;
+		a=100;
+		b=1;
+		c=b;
+		b=a;
+		a=c;
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		System.out.println("c="+c);
+
+	}
+
+}
