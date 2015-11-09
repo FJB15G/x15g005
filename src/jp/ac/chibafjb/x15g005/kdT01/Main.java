7ffe3c39d36852b89f7955b44938a2c18d76cd78
unknown <x15g005@PC504N017.chiba-fjb.ac.jp>
2015-11-09 14:20:49 +0900

    課題１８

---
+++ src/jp/ac/chibafjb/x15g005/kdT01/Main.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g005.kdT01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		for(i=1;i<=3;i+=1)
+			System.out.println(i);
+	}
+
+}
