78e6450a787d40d0b79f408484c7873b5941681b
unknown <x15g005@PC504N017.chiba-fjb.ac.jp>
2015-11-09 14:58:23 +0900

    課題２８

---
+++ src/jp/ac/chibafjb/x15g005/kd138/Main.java
@@ -0,0 +1,25 @@
+package jp.ac.chibafjb.x15g005.kd138;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a;
+		int b;
+		int c;
+		int d;;
+		a=1;
+		b=2;
+		c=3;
+		d=a;
+		a=b;
+		b=c;
+		c=d;
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		System.out.println("c="+c);
+		
+
+	}
+
+}
