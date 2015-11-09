9d602d17e311a971dc81f01f772a68aee7b037bc
unknown <x15g005@PC504N017.chiba-fjb.ac.jp>
2015-11-09 15:05:22 +0900

    課題２９

---
+++ src/jp/ac/chibafjb/x15g005/kd141/Main.java
@@ -0,0 +1,17 @@
+package jp.ac.chibafjb.x15g005.kd141;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.print("整数を一つ入力してください→");
+		Scanner sin=new Scanner(System.in);
+		String s=sin.next();
+		System.out.println("あなたの入力した数は"+s+"です。");
+		
+
+	}
+
+}
