caa725ec901bb9a239cf100e5f0f15969223caa8
unknown <x15g005@PC504N017.chiba-fjb.ac.jp>
2015-11-09 14:47:35 +0900

    課題２５

---
+++ src/jp/ac/chibafjb/x15g005/kd135/Main.java
@@ -0,0 +1,18 @@
+package jp.ac.chibafjb.x15g005.kd135;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String Str1;
+		String Str2;
+		Str1="apple";
+		Str2="orange";
+		System.out.println("Str1="+Str1);
+		System.out.println("Str2="+Str2);
+		
+		
+
+	}
+
+}
