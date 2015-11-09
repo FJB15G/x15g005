a0d86bb11a6a36958168021ab24ec4f8a698db6f
unknown <x15g005@PC504N017.chiba-fjb.ac.jp>
2015-11-09 14:35:00 +0900

    課題２３

---
+++ src/jp/ac/chibafjb/x15g005/kd133/Main.java
@@ -0,0 +1,18 @@
+package jp.ac.chibafjb.x15g005.kd133;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x;
+		int y;
+		x=10;
+		y=5;
+		int i;
+		i=x+y;
+		System.out.print("x+y="+i);
+		
+
+	}
+
+}
