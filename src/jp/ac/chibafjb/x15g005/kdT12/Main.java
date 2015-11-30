200214bcd1443e0a2a4ca250596ae3a2435b7107
unknown <x15g005@PC504N017.chiba-fjb.ac.jp>
2015-11-30 13:58:06 +0900

    課題３７

---
+++ src/jp/ac/chibafjb/x15g005/kdT12/Main.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g005.kdT12;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for(int i=30;i>=20;i-=5){
+			System.out.print(i+"　");
+		}
+
+	}
+
+}
