893c6eac2b586db481143adacdf35a9f70829253
unknown <x15g005@PC504N017.chiba-fjb.ac.jp>
2015-11-30 15:06:56 +0900

    課題４１

---
+++ src/jp/ac/chibafjb/x15g005/kd162/Main.java
@@ -0,0 +1,16 @@
+package jp.ac.chibafjb.x15g005.kd162;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x[]={33,18,102,47,5};
+		int a=0;
+		for(int i=0;i<x.length;i++){
+			a=a+x[i];
+		}
+		System.out.println("合計　＝ "+a);
+
+	}
+
+}
