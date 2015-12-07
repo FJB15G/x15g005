7c0e55e1af8818f6307296685fe4b6b03c95e726
unknown <x15g005@PC504N017.chiba-fjb.ac.jp>
2015-12-07 14:01:59 +0900

    課題４３

---
+++ src/jp/ac/chibafjb/x15g005/kd164/Main.java
@@ -0,0 +1,15 @@
+package jp.ac.chibafjb.x15g005.kd164;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int b[]={5,10,15};
+		int m=3;
+		for(int i=0;i<m;i++){
+			System.out.println("b["+i+"]"+"="+b[i]);
+		}
+
+	}
+
+}
