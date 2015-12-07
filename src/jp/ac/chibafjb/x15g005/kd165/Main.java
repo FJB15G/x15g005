69a621e19baefc213012e2da2099509f5393e72a
unknown <x15g005@PC504N017.chiba-fjb.ac.jp>
2015-12-07 14:20:00 +0900

    課題４４

--- src/jp/ac/chibafjb/x15g005/kd165/Main.java
+++ src/jp/ac/chibafjb/x15g005/kd165/Main.java
@@ -4,7 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		
+		int a[]={5,10,15};
+		int b[]={1,2,3};
+		int m=2;
+		for(int i=0;i<=m;i++){
+			b[i]=a[m-i];
+			System.out.println("b["+i+"]"+"="+b[i]);
+		}
 
 	}
 
