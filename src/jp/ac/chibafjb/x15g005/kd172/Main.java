2ec306cad680f9f8d55033e9cceb01bc0522ce94
unknown <x15g005@PC504N017.chiba-fjb.ac.jp>
2015-12-14 15:08:12 +0900

    課題４５

--- src/jp/ac/chibafjb/x15g005/kd172/Main.java
+++ src/jp/ac/chibafjb/x15g005/kd172/Main.java
@@ -1,16 +1,20 @@
-package jp.ac.chibafjb.x15g005.kd164;
+package jp.ac.chibafjb.x15g005.kd172;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		int a[]={5,10,15};
-		int b[]={1,2,3};
+		int x[]={1,2,3};
+		int y[]={7,8};
+		int z[]={};
 		int m=3;
+		int n=2;
 		for(int i=0;i<m;i++){
-			b[i]=a[i];
-			System.out.println("b["+i+"]"+"="+a[a]);
+			z[i]=x[i];
 		}
+		
+		System.out.println(x[]);
+		
 
 	}
 
